7a159abc207627dd1bca8bbfd7ada21621894d16
cidy02 <yccid02@gmail.com>
2026-08-03 10:13:24 -0400

    Android: bump version to 0.1.3 (versionCode 4) for release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "io.github.cidy02.kudos"
         minSdk = 26
         targetSdk = 37
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.1.3"
     }
 
     signingConfigs {
